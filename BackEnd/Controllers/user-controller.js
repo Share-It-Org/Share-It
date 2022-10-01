e93d590190f12c121e14eeb38670faacaff88c4a
drewdunne <57418947+drewdunne@users.noreply.github.com>
2022-10-01 12:46:36 -0700

    Basic Routing in place for User

---
+++ BackEnd/Controllers/user-controller.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const db = require('../Models/')
+
+const userController = {};
+
+userController.createUser = (req, res, next) => {
+
+}
+
+
+
+
+module.exports = userController;
