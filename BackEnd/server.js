067148e176c7df022e56801754a8d2b4cb3b4f39
zihaoli518 <zihaoli518@gmail.com>
2022-10-01 13:35:53 -0700

    Setting up server.js and userController.js for implementing login feature

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -6,6 +6,8 @@ const path = require('node:path');
 const express = require('express');
 const app = express();
 
+//importing controllers 
+const userController = require('./userController')      
 
 //test response for initial functionality. 
 app.get('/test', 
@@ -13,7 +15,10 @@ app.get('/test',
     response.status(200).send('Testing Testing, express is functioning. Is nodemon? Yes. ')
 });
 
-
+app.post('/login', userController,
+  (request, response) => {
+    response.status(200).send('testing login function')
+  })
 
 app.listen(3000, () => {
   console.log('Server is Listening on port 3000');
