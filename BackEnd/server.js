db2219f5129ab1e5fc45e2c6e5ebb3bacd6e5025
Vardan <vardan.vanyan@gmail.com>
2022-10-01 12:24:56 -0700

    made changes,deleted unwanted comments

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,18 +2,15 @@
 //path setup
 const path = require('node:path');
 //express stuff
-//import express from 'express';            //this doesn't work.
 const express = require('express');
 const app = express();
 
-
-//test response for initial functionality. 
-app.get('/test', 
-  (request, response) => {
-    response.status(200).send('Testing Testing, express is functioning. Is nodemon? Yes. ')
+//test response for initial functionality.
+app.get('/test', (request, response) => {
+  response
+    .status(200)
+    .send('Testing Testing, express is functioning. Is nodemon? Yes. ');
 });
-
-
 
 app.listen(3000, () => {
   console.log('Server is Listening on port 3000');
