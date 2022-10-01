2d77318829d4389025e02b575b214b1d32aada86
Vardan <vardan.vanyan@gmail.com>
2022-09-30 20:16:50 -0700

    added nodemon,css,images,express

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,44 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
-
+  entry: './FrontEnd/react-index.js',
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
   },
-
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './src/index.html',
+      template: './FrontEnd/react-index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        //handling react code with JS and JSX
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      //css
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      //loading images
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
