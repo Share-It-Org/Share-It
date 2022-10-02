0c0f4cdab283d88542d56adcf8a1983560892c7d
zihaoli518 <zihaoli518@gmail.com>
2022-10-01 17:47:55 -0700

    Updated middleware functions in user-controller to be compatible with database-controller

--- BackEnd/Controllers/user-controller.js
+++ BackEnd/Controllers/user-controller.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const db = require('../Models/database-model')
+// setting up bcrypt stuff
+const bcrypt = require('bcrypt');
+const saltFactor = 10; 
+
 
 const userController = {};
 
@@ -10,7 +14,11 @@ userController.createUser = (req, res, next) => {
     
     req.locals.queryData.tableName = 'test3';
     req.locals.queryData.username = req.body.username;
-    req.locals.queryData.password = req.body.password;
+
+    // req.locals.queryData.password = req.body.password;
+    bcrypt.hash(req.body.password, saltFactor, function(error, hash) {
+        req.locals.queryData.password = hash;
+      })
 
     console.log(req.body);
     console.log(req.locals);
@@ -18,4 +26,26 @@ userController.createUser = (req, res, next) => {
 
     next();
 }
+
+// this middleware should be chained AFTER database-controller-get user middleware
+// expects the hashed password retrieved from database to be stored in req.locals.hashedPassword 
+userController.loginUser = (req, res, next) => {
+  // get username and password from request body
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const hashedPassword = req.locals.hashedPassword;
+  bcrypt.compare(password, hashedPassword, function (error, result) {
+    if (result) {
+      console.log('its a match!')
+      return next();
+    }
+    return next({error})
+  });
+
+};
+
+
+
+
 module.exports = userController;
