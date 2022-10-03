3668c5d879883aa4a0620b91cf55c7c4ce57f4b7
Drew Dunne <drewdunne88@gmail.com>
2022-10-03 13:55:17 -0700

    <Improvement> Added database to router for items

--- BackEnd/Routes/item-router.js
+++ BackEnd/Routes/item-router.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const databaseController = require('../Controllers/database-controller');
 const itemController = require('../Controllers/item-controllers');
 const router = express.Router();
 
-router.post('/create', itemController.createItem, (req, res, next) => {
+router.post('/create', itemController.createItem, databaseController.insertRecord, (req, res, next) => {
     //Handle Create Item Response Here
     res.status(418).send("Not Yet Implemented");
 })
 
-router.get('/get', itemController.getItems, (req, res, next) => {
+router.get('/get', itemController.getItems, databaseController.getRecords, (req, res, next) => {
     //Handle Create Item Response Here
     res.status(418).send("Not Yet Implemented");
 })
