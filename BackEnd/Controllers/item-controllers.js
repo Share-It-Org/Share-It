58e454cf80759042844dca44686bdb8abeaa0230
Vardan <vardan.vanyan@gmail.com>
2022-10-03 14:11:24 -0700

    adding to creatItem

--- BackEnd/Controllers/item-controllers.js
+++ BackEnd/Controllers/item-controllers.js
@@ -10,7 +10,10 @@ itemController.createItem = (req, res, next) => {
 
   req.locals.queryData.tableName = 'item';
   req.locals.queryData.name = req.body.name;
+  //if we need description of an item
+//   req.locals.queryData.description = req.body.description;
 
+  console.log(req.body);
   next();
 };
 
