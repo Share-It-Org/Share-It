893da69999cd4af15ecee202c048d3f31c870c83
Vardan <vardan.vanyan@gmail.com>
2022-10-03 13:41:29 -0700

    adding item create

---
+++ BackEnd/Controllers/item-controllers.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const db = require('../Models/database-model');
+
+const itemController = {};
+
+itemController.createItem = (req, res, next) => {
+    
+};
+
+module.exports = itemController;
