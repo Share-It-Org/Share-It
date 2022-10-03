1934cf0bbd808d4ed99d93724b9aea1ce48efb1e
Tony Diethelm <tonydiethelm@gmail.com>
2022-10-03 12:14:52 -0700

    User create working! can submit user on signup page, shows up in DB. No password! We need to fix that.

--- FrontEnd/components/Signup.jsx
+++ FrontEnd/components/Signup.jsx
@@ -2,19 +2,31 @@
 //import everything, set up default variables, etc etc etc.
 import React from 'react';
 
-function Signup({consoleLogForTesting}){
+function handleClick(event, sendACreateUserRequest) {
+    event.preventDefault();
+    // let usernameText = document.getElementById('firstName')
+    // let passwordText = document.getElementById('password')
+    // let emailText = document.getElementById('email')
+    // console.log(usernameText);
+    // sendACreateUserRequest(usernameText, passwordText, emailText);
+}
+
+function Signup({sendACreateUserRequest}){
+    let username = '';
+    let password = '';
+    let email = '';
 
     return <div id='signUpBox' className='centerMe'>
         <h1>Please create an account</h1>
-    <form onSubmit={consoleLogForTesting}>
+    <form onSubmit={(event) => sendACreateUserRequest(event, username, password, email)}>
         <label>Username:</label>
-        <input type="text" id="fname" name="fname"></input>
+        <input type="text" id="firstName" name="firstName" onChange={(e) => username = e.target.value}></input>
         <br></br>
         <label>Password:</label>
-        <input type="text" id="lname" name="lname"></input>
+        <input type="text" id="password" name="password" onChange={(e) => password = e.target.value}></input>
         <br></br>
         <label>Email:</label>
-        <input type="text" id="lname" name="lname"></input>
+        <input type="text" id="email" name="email" onChange={(e) => email = e.target.value}></input>
         <br></br>
         <input type="submit" value="Submit"></input>
     </form>
