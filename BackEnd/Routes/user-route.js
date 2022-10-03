0b0208a52463e3b1fcfed64334b590b89b515f83
Drew Dunne <drewdunne88@gmail.com>
2022-10-03 16:20:22 -0700

    Fixed Create User Route

--- BackEnd/Routes/user-route.js
+++ BackEnd/Routes/user-route.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const databaseController = require('../Controllers/database-controller');
 const userController = require('../Controllers/user-controller');
@@ -9,10 +10,12 @@ router.post('/create', userController.createUser, databaseController.insertRecor
     res.sendStatus(200);
 })
 
-router.post('/login', userController.loginUser, databaseController.getRecords, (request, response) => {
-    response.status(200).send('testing login function')
+router.post('/login', userController.loginUserBefore, databaseController.getRecords, userController.loginUserAfter, (request, response) => {
+    response.status(200).send('testing login function');
   });
 
-router.post('/signup')
+router.post('/signup', userController.createUser, databaseController.insertRecord, (req, res) => {
+  response.status(200).send("Created user!");
+})
 
 module.exports = router;
