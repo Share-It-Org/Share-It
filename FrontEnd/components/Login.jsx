8e3f78f0f70ae68af23bd7101d82cd7813f798fe
Tony Diethelm <tonydiethelm@gmail.com>
2022-10-04 08:46:18 -0700

    saving last bit of work

--- FrontEnd/components/Login.jsx
+++ FrontEnd/components/Login.jsx
@@ -10,10 +10,10 @@ function Login({ sendALoginRequest, goToCreateUser }){
         <h1>Please log in </h1>
         <form onSubmit={(event) => sendALoginRequest(event, username, password)}>
             <label>Username:</label>
-            <input type="text" id="username" name="username" onChange={(e) => username = e.target.value}></input>
+            <input type="text" id="username" name="username" onChange={(event) => username = event.target.value}></input>
             <br></br>
             <label >Password:</label>
-            <input type="text" id="password" name="password" onChange={(e) => password = e.target.value}></input>
+            <input type="text" id="password" name="password" onChange={(event) => password = event.target.value}></input>
             <br></br>
             <input type="submit" value="Submit"></input>
         </form>
