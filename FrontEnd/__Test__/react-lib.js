78fe84de2f7574abad4ce97669d2f2cafdcedaf7
forestleigh <theforestleigh@gmail.com>
2022-10-05 16:53:47 -0700

    checked puppeteer tests and now moving to react library tests

---
+++ FrontEnd/__Test__/react-lib.js
@@ -0,0 +1,6 @@
+import React from 'React';
+// import userEvent from '@testing-library/user-event'
+import { render, screen, waitFor } from '@testing-library/react';
+import regeneratorRuntime from 'regenerator-runtime';
+
+import App from '../Fgit d/components/Card';
